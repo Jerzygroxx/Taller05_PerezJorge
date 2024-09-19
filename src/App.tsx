8fee8bde79cd16894487da57209f245cd8b3d7da
jorgeluisperez1006 <jorgeluisperez1006@hotmail.com>
2024-09-19 11:51:42 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css"
+import LogoU from './assets/img/Logo_cuc.png'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <img src ={LogoU} alt ="logo de React" />
+        <h1>Bienvenidos a la Universidad de la costa</h1>
+        <p>Nos complace presentarnos</p>
+        </div>
+    </>
+  )
+}
+
+export default App
